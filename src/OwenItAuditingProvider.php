7214e92e972834b653dd8e515686709704208ca7
Antério <anteriovieira@gmail.com>
2015-08-17 17:46:21 -0300

    Adicionado service provider

---
+++ src/OwenItAuditingProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OwenIt\Auditing;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * This is the owen auditing service provider class.
+ *
+ */
+class AuditingServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setupMigrations();
+    }
+
+    /**
+     * Setup the migrations.
+     *
+     * @return void
+     */
+    protected function setupMigrations()
+    {
+        $source = realpath(__DIR__.'/migrations/');
+
+        $this->publishes([$source => database_path('migrations')], 'migrations');
+    }
+
+}
