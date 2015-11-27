470ac9616a100cc8bdf80651caf29992fb895e37
Antério vieira da silva <anteriovieira@gmail.com>
2015-11-27 17:38:02 -0200

    Fixed autoload and namespace

---
+++ config/auditing.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of laravel-auditing.
+ *
+ * @author Antério Vieira <anteriovieira@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [];
---
+++ src/AuditingServiceProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace OwenIt\Auditing;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApp;
+
+/**
+ * This is the owen auditing service provider class.
+ */
+class AuditingServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->setupMigrations($this->app);
+//        $this->setupConfig($this->app);
+    }
+
+    /**
+     * Setup the config.
+     *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function setupConfig(Application $app)
+    {
+        $source = realpath(__DIR__.'/../config/auditing.php');
+
+        if ($app instanceof LaravelApp && $app->runningInConsole()) {
+            $this->publishes([$source => config_path('auditing.php')]);
+        }
+
+        $this->mergeConfigFrom($source, 'auditing');
+    }
+
+    /**
+     * Setup the migrations.
+     *
+     * @return void
+     */
+    protected function setupMigrations(Application $app)
+    {
+        $source = realpath(__DIR__.'/../database/migrations/');
+
+        if ($app instanceof LaravelApp && $app->runningInConsole()) {
+            $this->publishes([$source => database_path('migrations')], 'migrations');
+        }
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
