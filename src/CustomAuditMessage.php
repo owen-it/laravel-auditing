7757b861b902e9a1cc8bbe26124226fcd52a9793
Paul Kievits <kievits@rsm.nl>
2016-11-11 10:15:02 +0100

    Added some clearer messages for relations

--- src/CustomAuditMessage.php
+++ src/CustomAuditMessage.php
@@ -207,17 +207,19 @@ trait CustomAuditMessage
     {
         // Get the related type, it's guestimation so if this isn't providing what you want simply override this function
         if (in_array($this->type, ['attached', 'updatedRelation', 'detached'])) {
-            $relatedType = strtolower(str_replace('\\', '', substr($this->related_type, strrpos($this->related_type, '\\'))));
+            $relatedType = str_replace('\\', '', substr($this->related_type, strrpos($this->related_type, '\\'))); // Get the full name
+            $relatedType = implode(' ', preg_split('/(?=[A-Z])/',$relatedType)); // Replace camel case with spaces
+            $relatedType = strtolower($relatedType); // lower case
 
             switch ($this->type) {
                 case 'attached':
-                    return "attached a {$relatedType} to";
+                    return "attached a {$relatedType} with ID {$this->related_id} to";
                     break;
                 case 'updatedRelation':
-                    return "updated a related {$relatedType} attached to";
+                    return "updated a related {$relatedType} with ID {$this->related_id} attached to";
                     break;
                 case 'detached':
-                    return "detached a {$relatedType} from";
+                    return "detached a {$relatedType} with ID {$this->related_id} from";
                     break;
             }
         }
