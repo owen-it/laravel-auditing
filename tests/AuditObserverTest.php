9d26aa41f680a9fd015698ff63e5e6e4796ba80d
Quetzy Garcia <quetzyg@altek.org>
2016-11-27 09:26:48 +0000

    fix(*): observer tests

--- tests/AuditObserverTest.php
+++ tests/AuditObserverTest.php
@@ -1,40 +1,41 @@
 <?php
 
-namespace Tests;
+namespace OwenIt\Auditing\Tests;
 
 use Mockery;
-use OwenIt\Auditing\AuditObserver;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Observers\Audit;
 
 class AuditObserverTest extends AbstractTestCase
 {
     public function test_saving_handler_prepare_audit()
     {
-        $observer = new AuditObserver();
-        $model = Mockery::mock();
+        $observer = new Audit();
+        $model = Mockery::mock(Auditable::class);
         $model->shouldReceive('prepareAudit');
         $observer->saving($model);
     }
 
     public function test_created_handler_audit_creation()
     {
-        $observer = new AuditObserver();
-        $model = Mockery::mock();
+        $observer = new Audit();
+        $model = Mockery::mock(Auditable::class);
         $model->shouldReceive('auditCreation');
         $observer->created($model);
     }
 
     public function test_saved_handler_audit_update()
     {
-        $observer = new AuditObserver();
-        $model = Mockery::mock();
+        $observer = new Audit();
+        $model = Mockery::mock(Auditable::class);
         $model->shouldReceive('auditUpdate');
         $observer->saved($model);
     }
 
     public function test_deleted_handler_audit_deletion()
     {
-        $observer = new AuditObserver();
-        $model = Mockery::mock();
+        $observer = new Audit();
+        $model = Mockery::mock(Auditable::class);
         $model->shouldReceive('prepareAudit');
         $model->shouldReceive('auditDeletion');
         $observer->deleted($model);
