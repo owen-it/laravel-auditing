c0366204c4932bdcdb1e181a1ebb2057019c3e00
Quetzy Garcia <quetzyg@altek.org>
2016-12-03 22:47:59 +0000

    fix(Tests): added a simple test method to each class

--- tests/AuditModelTest.php
+++ tests/AuditModelTest.php
@@ -2,7 +2,21 @@
 
 namespace OwenIt\Auditing\Tests;
 
+use OwenIt\Auditing\Models\Audit;
+
 class AuditModelTest extends AbstractTestCase
 {
-    //
+    /**
+     * Test the Audit class instantiation.
+     *
+     * @return Audit
+     */
+    public function testAuditInstantiation()
+    {
+        $audit = new Audit();
+
+        $this->assertInstanceOf(Audit::class, $audit);
+
+        return $audit;
+    }
 }
--- tests/AuditableTest.php
+++ tests/AuditableTest.php
@@ -2,7 +2,22 @@
 
 namespace OwenIt\Auditing\Tests;
 
+use Mockery;
+use OwenIt\Auditing\Contracts\Auditable;
+
 class AuditableTest extends AbstractTestCase
 {
-    //
+    /**
+     * Test the Auditable class instantiation.
+     *
+     * @return Auditable
+     */
+    public function testAuditableInstantiation()
+    {
+        $model = Mockery::mock(Auditable::class);
+
+        $this->assertInstanceOf(Auditable::class, $model);
+
+        return $model;
+    }
 }
