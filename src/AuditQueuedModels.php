9c1cfaf35a1a22c16a637fd1d2e5387f041e6e71
Khaled Sameer <khaled.smq@hotmail.com>
2017-02-02 14:22:06 +0400

    add forgotten namespaces
    add forgotten namespaces

--- src/AuditQueuedModels.php
+++ src/AuditQueuedModels.php
@@ -3,6 +3,8 @@
 namespace OwenIt\Auditing;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
 class AuditQueuedModels implements ShouldQueue
