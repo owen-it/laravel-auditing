32118637bc089e3d613a3183982b3285ee9f8ee8
Quetzy Garcia <quetzyg@altek.org>
2017-03-05 12:38:50 +0000

    feat(AuditableModelStub): override resolveIpAddress() method

--- tests/Stubs/AuditableModelStub.php
+++ tests/Stubs/AuditableModelStub.php
@@ -9,4 +9,12 @@ use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 class AuditableModelStub extends Model implements AuditableContract
 {
     use Auditable;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveIpAddress()
+    {
+        return '127.0.0.1';
+    }
 }
