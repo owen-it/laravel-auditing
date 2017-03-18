c30d211fcdafab6911b3d208788fddf3f9ea5c15
Quetzy Garcia <quetzyg@altek.org>
2017-03-18 17:18:21 +0000

    feat(UserResolver): initial implementation

---
+++ src/Contracts/UserResolver.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the Laravel Auditing package.
+ *
+ * @author     Antério Vieira <anteriovieira@gmail.com>
+ * @author     Quetzy Garcia  <quetzyg@altek.org>
+ * @author     Raphael França <raphaelfrancabsb@gmail.com>
+ * @copyright  2015-2017
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
+namespace OwenIt\Auditing\Contracts;
+
+interface UserResolver
+{
+    /**
+     * Resolve the ID of the logged User.
+     *
+     * @return mixed|null
+     */
+    public static function resolveId();
+}
