2c8fb1a217a07183274a2f07f4be8dc0f2ded5b7
Quetzy Garcia <quetzyg@altek.org>
2017-07-03 20:49:52 +0100

    feat(Auditable): get the Audit implementation from the config

--- src/Auditable.php
+++ src/Auditable.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Str;
 use OwenIt\Auditing\Contracts\UserResolver;
-use OwenIt\Auditing\Contracts\Audit as AuditContract;
 use RuntimeException;
 use UnexpectedValueException;
 
@@ -52,13 +51,11 @@ trait Auditable
     }
 
     /**
-     * Auditable Model audits.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     * {@inheritdoc}
      */
     public function audits()
     {
-        return $this->morphMany(AuditContract::class, 'auditable');
+        return $this->morphMany(Config::get('audit.implementation'), 'auditable');
     }
 
     /**
