dc1b80926f5cbaaccc90a203b12144b9ff5621c5
Quetzy Garcia <quetzyg@altek.org>
2017-11-04 00:03:19 +0000

    feat(Tests): implemented AuditingTestCase

---
+++ tests/AuditingTestCase.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of the Laravel Auditing package.
+ *
+ * @author     Antério Vieira <anteriovieira@gmail.com>
+ * @author     Quetzy Garcia  <quetzyg@altek.org>
+ * @author     Raphael França <raphaelfrancabsb@gmail.com>
+ * @copyright  2015-2017
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
+namespace OwenIt\Auditing\Tests;
+
+use Orchestra\Database\ConsoleServiceProvider;
+use Orchestra\Testbench\TestCase;
+use OwenIt\Auditing\AuditingServiceProvider;
+use OwenIt\Auditing\Tests\Models\User;
+
+class AuditingTestCase extends TestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Database
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        // Auditing
+        $app['config']->set('audit.user.model', User::class);
+        $app['config']->set('audit.user.resolver', User::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->withFactories(__DIR__ . '/database/factories');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            AuditingServiceProvider::class,
+            ConsoleServiceProvider::class
+        ];
+    }
+}
