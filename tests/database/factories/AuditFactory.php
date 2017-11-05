19f798999f0560bfcd3d5dd6e2a617465a35e491
Quetzy Garcia <quetzyg@altek.org>
2017-11-05 02:41:06 +0000

    feat(Tests): implement Audit factory

---
+++ tests/database/factories/AuditFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+use Faker\Generator as Faker;
+use OwenIt\Auditing\Models\Audit;
+use OwenIt\Auditing\Tests\Models\Article;
+use OwenIt\Auditing\Tests\Models\User;
+/*
+|--------------------------------------------------------------------------
+| Audit Factories
+|--------------------------------------------------------------------------
+|
+*/
+
+$factory->define(Audit::class, function (Faker $faker) {
+    return [
+        'user_id'        => function () {
+            return factory(User::class)->create()->id;
+        },
+        'event'          => 'updated',
+        'auditable_id'   => function () {
+            return factory(Article::class)->create()->id;
+        },
+        'auditable_type' => Article::class,
+        'old_values'     => [],
+        'new_values'     => [],
+        'url'            => $faker->url,
+        'ip_address'     => $faker->ipv4,
+        'user_agent'     => $faker->userAgent,
+        'tags'           => implode(',', $faker->words(4)),
+    ];
+});
