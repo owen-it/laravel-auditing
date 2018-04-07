e3fd3ba5bf1605f723b3d6fa26e083b780bdbbd3
Quetzy Garcia <quetzyg@altek.org>
2018-04-07 20:09:58 +0100

    feat(AuditRedactors): initial implementation

---
+++ src/Contracts/AuditRedactor.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the Laravel Auditing package.
+ *
+ * @author     Antério Vieira <anteriovieira@gmail.com>
+ * @author     Quetzy Garcia  <quetzyg@altek.org>
+ * @author     Raphael França <raphaelfrancabsb@gmail.com>
+ * @copyright  2015-2018
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
+namespace OwenIt\Auditing\Contracts;
+
+interface AuditRedactor
+{
+    /**
+     * Redact a value.
+     *
+     * @param mixed $value
+     *
+     * @return string
+     */
+    public static function redact($value): string;
+}
