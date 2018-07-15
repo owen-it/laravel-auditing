a2309c07ec425cf599d7944ca039ce872ffa48bb
Quetzy Garcia <quetzyg@altek.org>
2018-07-15 10:40:43 +0100

    feat(Encoder): implement Base64Encoder

---
+++ src/Encoders/Base64Encoder.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Laravel Auditing package.
+ *
+ * @author     Antério Vieira <anteriovieira@gmail.com>
+ * @author     Quetzy Garcia  <quetzyg@altek.org>
+ * @author     Raphael França <raphaelfrancabsb@gmail.com>
+ * @copyright  2015-2018
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE.md file that was distributed
+ * with this source code.
+ */
+
+namespace OwenIt\Auditing\Encoders;
+
+class Base64Encoder implements \OwenIt\Auditing\Contracts\AttributeEncoder
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function encode($value): string
+    {
+        return base64_encode($value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function decode($value): string
+    {
+        return base64_decode($value);
+    }
+}
