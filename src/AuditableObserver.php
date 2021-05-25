6d50604d82c6f6dbb3aa431022ac8edac929e73c
rcarvs <renancarvs@gmail.com>
2021-05-25 15:15:56 -0300

    file pattern

--- src/AuditableObserver.php
+++ src/AuditableObserver.php
@@ -25,8 +25,7 @@ class AuditableObserver
     {
         if ($model instanceof Auditable) {
             Auditor::execute($model->setAuditEvent('retrieved'));
-        }
-        
+        }       
     }
 
     /**
