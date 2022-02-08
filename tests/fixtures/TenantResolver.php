2df8c275f35b5ced84ca4fa64f7ba85dc02ba811
Morten D. Hansen <morten@visia.dk>
2022-02-08 22:53:23 +0100

    Fix test-resolver missing argument

--- tests/fixtures/TenantResolver.php
+++ tests/fixtures/TenantResolver.php
@@ -2,11 +2,13 @@
 
 namespace OwenIt\Auditing\Tests\fixtures;
 
+use OwenIt\Auditing\Contracts\Auditable;
 use OwenIt\Auditing\Contracts\Resolver;
 
 class TenantResolver implements Resolver
 {
-    public static function resolve()
+
+    public static function resolve(Auditable $auditable)
     {
         return 1;
     }
