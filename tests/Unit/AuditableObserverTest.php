cadccc176a4f6793882eac151a2c1c92d1e84728
Kevin Ullyott <ullyott.kevin@gmail.com>
2023-08-22 23:02:31 -0400

    Remove retrieved
    Signed-off-by: Kevin Ullyott <ullyott.kevin@gmail.com>

--- tests/Unit/AuditableObserverTest.php
+++ tests/Unit/AuditableObserverTest.php
@@ -134,9 +134,6 @@ class AuditableObserverTest extends AuditingTestCase
     {
         return [
             [
-                'retrieved',
-            ],
-            [
                 'created',
             ],
             [
