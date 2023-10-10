9559699c6d775ec4697fafff41338ed624eaeaa0
Giso Stallenberg <giso@unity-x.nl>
2023-10-10 11:22:17 +0200

    Read Audit implementation from MorphMany
    To allow a different Audit implementation **per model** the implementation should be deducted from the MorphMany given by the Auditable::audits method.
    
    This will allow having a main Audit implementation (which defaults to `OwenIt\Auditing\Models\Audit`), but also have a different implementation once the Auditable::audits method is overwritten.
    
    Example:
    ```
    class MyAuditable extends Model implements AuditableContract
    {
        use Auditable;
    
        /**
         * @return MorphMany<AuditMyAuditableModel>
         */
        public function audits(): MorphMany
        {
            return $this->morphMany(
                AuditMyAuditableModel::class,
                'auditable',
            );
        }
    ```

--- src/Drivers/Database.php
+++ src/Drivers/Database.php
@@ -14,9 +14,7 @@ class Database implements AuditDriver
      */
     public function audit(Auditable $model): ?Audit
     {
-        $implementation = Config::get('audit.implementation', \OwenIt\Auditing\Models\Audit::class);
-
-        return call_user_func([$implementation, 'create'], $model->toAudit());
+        return call_user_func([$model->audits()->getModel()::class, 'create'], $model->toAudit());
     }
 
     /**
