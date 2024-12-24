8b88302c707009f14ffee6676884c02c2861991b
William Hall <william.hall@synergitech.co.uk>
2024-12-24 16:47:11 +0000

    🐛 void function does not return

--- src/Contracts/Auditor.php
+++ src/Contracts/Auditor.php
@@ -6,17 +6,11 @@ interface Auditor
 {
     /**
      * Get an audit driver instance.
-     *
-     * @param \OwenIt\Auditing\Contracts\Auditable $model
-     *
-     * @return AuditDriver
      */
     public function auditDriver(Auditable $model): AuditDriver;
 
     /**
      * Perform an audit.
-     *
-     * @param \OwenIt\Auditing\Contracts\Auditable $model
      *
      * @return void
      */
