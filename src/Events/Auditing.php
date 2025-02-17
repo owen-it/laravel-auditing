fff281e1f83564344f200602d1430a49fa231ae3
William Hall <william.hall@synergitech.co.uk>
2025-02-17 18:02:28 +0000

    ♻️ php 8 constructor promotion

--- src/Events/Auditing.php
+++ src/Events/Auditing.php
@@ -8,24 +8,12 @@ use OwenIt\Auditing\Contracts\AuditDriver;
 class Auditing
 {
     /**
-     * The Auditable model.
-     *
-     * @var \OwenIt\Auditing\Contracts\Auditable
-     */
-    public $model;
-
-    /**
-     * Audit driver.
-     *
-     * @var \OwenIt\Auditing\Contracts\AuditDriver
-     */
-    public $driver;
-
-    /**
      * Create a new Auditing event instance.
      */
-    public function __construct(Auditable $model, AuditDriver $driver)
-    {
+    public function __construct(
+        public Auditable $model,
+        public AuditDriver $driver
+    ) {
         $this->model = $model;
         $this->driver = $driver;
     }
