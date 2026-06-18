65b7cb3451ef0835b9003287ee0de1bb5aa981d2
erikn69 <erikn_69@hotmail.com>
2026-06-18 08:28:02 -0500

    fix(test): rewrite test to properly validate functionality (#1057)

--- tests/Unit/OutsideOfAppContextTest.php
+++ tests/Unit/OutsideOfAppContextTest.php
@@ -2,14 +2,22 @@
 
 namespace OwenIt\Auditing\Tests\Unit;
 
+use Illuminate\Support\Facades\App;
 use OwenIt\Auditing\Tests\Models\Article;
+use PHPUnit\Framework\Attributes\PreserveGlobalState;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use PHPUnit\Framework\TestCase;
 
 class OutsideOfAppContextTest extends TestCase
 {
+    #[RunInSeparateProcess]
+    #[PreserveGlobalState(false)]
     public function test_can_create_new_model(): void
     {
+        $this->assertNull(App::getFacadeRoot());
+
         $article = new Article();
         $this->assertInstanceOf(Article::class, $article);
+        $this->assertNull(Article::getEventDispatcher());
     }
 }
